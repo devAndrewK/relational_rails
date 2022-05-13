909327850e880aaa1d5e48d9301dc5dd89603c06
Andrew King <dev.andrewking@gmail.com>
2022-05-13 10:30:00 -0500

    add model_spec with shoulda matchers

--- spec/models/tour_spec.rb
+++ spec/models/tour_spec.rb
@@ -1,1 +1,14 @@
 require 'rails_helper'
+
+RSpec.describe Tour, type: :model do
+    describe 'validations' do
+        it { should validate_presence_of :name }
+        it { should validate_presence_of :merch_on_hand }
+        it { should validate_presence_of :sold_out }
+    end
+
+    describe 'relationships' do
+        it { should have_many :venues }
+    end
+
+end
