78106d005943bedf7dab662a49091f871c9dea67
Andrew King <dev.andrewking@gmail.com>
2022-05-18 14:30:37 -0500

    add filter tour/venues index by threshold

--- app/controllers/tour_venues_controller.rb
+++ app/controllers/tour_venues_controller.rb
@@ -1,7 +1,11 @@
 class TourVenuesController < ApplicationController
     def index
+        if !params[:threshold]
+            params[:threshold] = 0
+        end
         tour = Tour.find(params[:tour_id])
-        @venues = tour.venues.order(params[:sort])
+        @venues = tour.venues.order(params[:sort]).
+        where("capacity > #{params[:threshold]}")
     end
 
     def new
--- spec/features/tours/venues/index_spec.rb
+++ spec/features/tours/venues/index_spec.rb
@@ -45,4 +45,24 @@ RSpec.describe 'Tours venues index' do
         expect(mohegan.name).to appear_before(pier.name)
     end
 
+    it "has a form to filter results under a threshold" do
+        gojira = Tour.create!(name: 'Gojira', sold_out: false, merch_on_hand: 1000)
+        rave = gojira.venues.create!(name: 'The Rave', capacity: 3500, all_ages: true, 
+            city: 'Milwaukee')
+        agganis = gojira.venues.create!(name: 'Agganis Arena', capacity: 3000, all_ages: true, 
+            city: 'Boston')
+        pier = gojira.venues.create!(name: 'Pier 17', capacity: 4000, all_ages: true, 
+            city: 'New York')
+        mohegan = gojira.venues.create!(name: 'Mohegan Sun Arena', capacity: 3500, all_ages: false, 
+            city: 'Washington, DC')
+
+        visit "/tours/#{gojira.id}/venues"
+        fill_in 'threshold', with: '3600'
+        click_button "Only return records more than `number` of `capacity`"
+        expect(page).to have_content(pier.name)
+        expect(page).to_not have_content(agganis.name)
+        expect(page).to_not have_content(rave.name)
+        expect(page).to_not have_content(mohegan.name)
+    end
+
 end
