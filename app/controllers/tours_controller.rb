2bcb21c3514dfd997ea9f5e72045d7cdf48ba6e5
Andrew King <dev.andrewking@gmail.com>
2022-05-18 15:09:14 -0500

    attain 100% simplecov

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -7,9 +7,7 @@ class ToursController < ApplicationController
     end
 
     def create
-        tour = Tour.create(name: params[:name],
-                            merch_on_hand: params[:merch_on_hand],
-                            sold_out: params[:sold_out])
+        tour = Tour.create!(tour_params)
       
         redirect_to "/tours"
     end
--- spec/features/venues/edit_spec.rb
+++ spec/features/venues/edit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'edit a venue' do
   it 'can go to venue show and edit a venue' do
     gojira = Tour.create!(name: 'Gojira', sold_out: false, merch_on_hand: 1000)
-    agganis = gojira.venues.create!(name: 'Agganis Arena', capacity: 3000, all_ages: true, 
+    agganis = gojira.venues.create!(name: 'Agganis Arena', capacity: 3000, all_ages: false, 
         city: 'Boston')
 
     visit "/venues/#{agganis.id}"
@@ -13,6 +13,7 @@ RSpec.describe 'edit a venue' do
     expect(current_path).to eq("/venues/#{agganis.id}/edit")
 
     fill_in 'name', with: 'Agganis Stadium'
+    choose 'all_ages_true'
 
     click_button 'Update Venue'
     expect(current_path).to eq("/venues/#{agganis.id}")
